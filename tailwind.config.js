0743330ccfd1b42da0a1136ef586c84a8be5e624
LavaWaffle <60158215+LavaWaffle@users.noreply.github.com>
2022-03-08 18:34:24 -0500

    added purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 const tailwindCssAnimista = require("tailwindcss-animistacss")
 
 module.exports = {
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
